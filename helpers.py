0efc9415f0479f5de6ecb6a52be586b9916bf63c
mei <yimei.wym@gmail.com>
2019-05-03 15:25:35 +0800

    Delete WTForms and add form validation

---
+++ helpers.py
@@ -0,0 +1,36 @@
+from peewee_validates import ModelValidator, validate_length
+from models.user import User
+
+
+class FormValidator(ModelValidator):
+    class Meta:
+        messages = {
+            'email.required': 'Email is required',
+            'password.required': 'Password is required',
+            'username.required': 'Username is required',
+            'email.unique': 'An account is already registered to this email',
+            'username.unique': 'Username taken'
+        }
+
+
+def valid_length(field, min, max):
+    """Checks if length requirements are fulfilled"""
+    length = len(field)
+    if length >= min and length <= max:
+        return True
+    elif length < min:
+        return "too short"
+    elif length > max:
+        return "too long"
+
+
+def length_validation(**fields):
+    errors = {}
+    for key, value in fields.items():
+        res = valid_length(value, 6, 255)
+        # Checks length of username, password
+        if res == True:
+            pass
+        else:
+            errors[key] = f"{key} is {res}"
+    return errors
--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, render_template, request
-from instagram_web.forms.forms import *
+from flask import Blueprint, redirect, render_template, request, url_for
+from helpers import *
+# from instagram_web.forms.forms import *
 from models.user import *
+from werkzeug.security import check_password_hash, generate_password_hash
+from peewee_validates import ModelValidator, StringField, validate_length
 
 
 users_blueprint = Blueprint('users',
@@ -15,16 +18,41 @@ def new():
 
 @users_blueprint.route('/', methods=['POST'])
 def create():
-    # This is where I define user creation
-    form = RegistrationForm(request.form)
+    username = request.form.get("username")
+    email = request.form.get("email")
+    ori_password = request.form.get("password")
+    errors = []
 
-    if form.validate():
-        # Hash my password
+    # Check the length of the username, email and ori_password
+    length_errors = length_validation(Username=username, Email=email,
+                                      Password=ori_password)
+    for key, value in length_errors.items():
+        errors.append(value)
+
+    # Check if passwords match
+    if ori_password != request.form.get("confirm"):
+        errors.append("Passwords do not match")
+
+    # If errors is an empty array, i.e., there are no errors
+    if not errors:
+        # Hash user password
+        password = generate_password_hash(
+            ori_password, method="pbkdf2:sha256", salt_length=8)
+
         # Create a new instance of a user
-        user = User(username=form.username.data,
-                    email=form.email.data, password=password)
+        user = User(username=username,
+                    email=email, password=password)
+        # Validation
+        validator = FormValidator(user)
+        # If validation is successful
+        if validator.validate():
+            user.save()
+            return redirect(url_for("users.new"))
+        # Else, append the error message
+        for key, value in validator.errors.items():
+            errors.append(value)
 
-    return render_template('new.html', form=form)
+    return render_template('users/new.html', errors=errors)
 
 
 @users_blueprint.route('/<username>', methods=["GET"])
