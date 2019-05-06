ead35ae2f424df9fedd0d1b625d5c2c05dbf36ad
mei <yimei.wym@gmail.com>
2019-05-06 13:58:13 +0800

    handle incorrect password

--- instagram_web/blueprints/sessions/views.py
+++ instagram_web/blueprints/sessions/views.py
@@ -15,33 +15,41 @@ def new():
 
 
 @sessions_blueprint.route("/", methods=["POST"])
-# Handle user log in by creating a new session
 def create():
+    # Handle user log in by creating a new session
     # Get the user input
     username = request.form.get("username")
+
     try:
         # Check if username exists
         user = User.get(User.username == username)
-    except:
-        # If username is not found, flash an error
-        flash("User not found")
-        return redirect(url_for("sessions.new"))
 
-    # Get passwords
-    else:
+        # Get passwords
         form_pw = request.form.get("password")
         user = User.get(User.username == username)
         db_pw = user.password
+
         # Compare user hash and db hash
-        # If valid, log user in
         if check_password_hash(db_pw, form_pw):
             # Allow user to log in
             if login_user(user):
                 # Redirect to profile page/home page
                 flash("Successfully logged in")
-                # HAVE A BETTER REDIRECT HERE
+
+                """HAVE A BETTER REDIRECT HERE"""
                 return redirect(url_for("sessions.new"))
+
             else:
                 flash('An error occurred')
+
+        else:
+            # If password is incorrect
+            flash("Incorrect password")
+            return redirect(url_for("sessions.new"))
+
+    except:
+        # If username is not found, flash an error
+        flash("User not found")
+        return redirect(url_for("sessions.new"))
 
     return render_template("sessions/new.html")
