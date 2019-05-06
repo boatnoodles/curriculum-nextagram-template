6e26fccd45cb0d3719519f3f8d7db82c5141b4e8
mei <yimei.wym@gmail.com>
2019-05-06 16:29:56 +0800

    register 404 and 500 error pages

--- instagram_web/__init__.py
+++ instagram_web/__init__.py
@@ -18,6 +18,15 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 @app.route("/")
 def home():
     return render_template('home.html')
+
+
+app.register_error_handler(404, page_not_found)
+app.register_error_handler(500, internal_server_error)
