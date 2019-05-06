08458fbde899bcbd5fa1f4cb4fd5d9ef8d0a14ba
mei <yimei.wym@gmail.com>
2019-05-06 15:21:04 +0800

    implement logout

--- instagram_web/blueprints/sessions/views.py
+++ instagram_web/blueprints/sessions/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, Flask, flash, redirect, render_template, request, session, url_for
-from flask_login import login_user
+from flask_login import login_user, logout_user
 from models.user import User
 from werkzeug.security import check_password_hash
 
@@ -13,7 +13,7 @@ def new():
     # Log in page
     return render_template("sessions/new.html")
 
-
+# Log user in
 @sessions_blueprint.route("/", methods=["POST"])
 def create():
     # Handle user log in by creating a new session
@@ -30,22 +30,20 @@ def create():
         db_pw = user.password
 
         # Compare user hash and db hash
-        if check_password_hash(db_pw, form_pw):
-            # Allow user to log in
-            if login_user(user):
-                # Redirect to profile page/home page
-                flash("Successfully logged in")
-
-                """HAVE A BETTER REDIRECT HERE"""
-                return redirect(url_for("sessions.new"))
-
-            else:
-                flash('An error occurred')
-
-        else:
+        if not check_password_hash(db_pw, form_pw):
             # If password is incorrect
             flash("Incorrect password")
             return redirect(url_for("sessions.new"))
+
+        # Allow user to log in
+        if not login_user(user):
+            flash('An error occurred')
+
+        # Redirect to profile page/home page
+        flash("Successfully logged in")
+
+        """HAVE A BETTER REDIRECT HERE"""
+        return redirect(url_for("sessions.new"))
 
     except:
         # If username is not found, flash an error
@@ -53,3 +51,11 @@ def create():
         return redirect(url_for("sessions.new"))
 
     return render_template("sessions/new.html")
+
+
+#
+@sessions_blueprint.route("/delete", methods=["POST"])
+def delete():
+    logout_user()
+    flash("Successfully logged out")
+    return redirect(url_for("home"))
