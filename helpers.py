d254f209273e5ec3823d0985cee3a811fd5d6ddb
mei <yimei.wym@gmail.com>
2019-05-07 13:16:02 +0800

    add boto helpers file

---
+++ helpers.py
@@ -0,0 +1,6 @@
+import boto3
+import botocore
+from config import S3_KEY, S3_SECRET, S3_BUCKET
+
+s3 = boto3.client("s3", aws_access_key_id=S3_KEY,
+                  aws_secret_access_key=S3_SECRET)
