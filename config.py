1cf842548b06dea9b79126715136521ce19c9ec6
mei <yimei.wym@gmail.com>
2019-05-07 13:11:13 +0800

    configure boto3

--- config.py
+++ config.py
@@ -30,3 +30,9 @@ class TestingConfig(Config):
     TESTING = True
     DEBUG = True
     ASSETS_DEBUG = True
+
+
+S3_BUCKET = os.environ.get("S3_BUCKET_NAME")
+S3_KEY = os.environ.get("S3_ACCESS_KEY")
+S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY")
+S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
