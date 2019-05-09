b8e91c0311d32484c314a441cc12204ba0287c7b
mei <yimei.wym@gmail.com>
2019-05-09 17:12:25 +0800

    set up donation views to handle donation
    1. Process donation using Braintree
    2. Save donation to db
    3. Send email if donation is successfully using SendGrid

--- instagram_web/blueprints/donations/views.py
+++ instagram_web/blueprints/donations/views.py
@@ -1,19 +1,59 @@
 import os
 import braintree
 from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_required
 from instagram_web.blueprints.donations.helpers import *
+from models.donation import Donation
+from models.post import Post
+from models.user import User
 
 donations_blueprint = Blueprint(
     'donations', __name__, template_folder='templates')
 
 
-@donations_blueprint.route('/new', methods=["GET"])
-def new():
+@donations_blueprint.route('/<recipient>/new', methods=["GET"])
+def new(recipient):
     client_token = generate_client_token()
-    return render_template('donations/new.html', client_token=client_token)
+    url = Post.get(Post.path == recipient).post_url
+    return render_template('donations/new.html', client_token=client_token, recipient=recipient, url=url)
 
 
-@donations_blueprint.route('/<transaction_id>', methods=['GET'])
+@donations_blueprint.route('/<recipient>', methods=['POST'])
+@login_required
+def create(recipient):
+    amount = request.form.get('amount')
+
+    result = transact({
+        'amount': amount,
+        'payment_method_nonce': request.form.get('payment_method_nonce'),
+        'options': {
+            "submit_for_settlement": True
+        }
+    })
+
+    # If transaction is successful
+    if result.is_success or result.transaction:
+        post = Post.get(Post.path == recipient)
+        recipient_email = User.get(User.id == post.user_id).email
+        # Save amount into database
+        q = Donation(donor=User.get_by_id(current_user.id),
+                     recipient_post=post, amount=amount)
+        if q.save():
+            # Display on the screen
+            # Send an email
+            send_email(recipient_email)
+            return redirect(url_for('donations.show', transaction_id=result.transaction.id))
+
+        else:
+            return redirect(url_for('donations.new', recipient=recipient))
+
+    else:
+        for x in result.errors.deep_errors:
+            flash('Error: %s: %s' % (x.code, x.message))
+        return redirect(url_for('donations.new'))
+
+
+@donations_blueprint.route('/show/<transaction_id>', methods=['GET'])
 def show(transaction_id):
     transaction = find_transaction(transaction_id)
     result = {}
@@ -31,23 +71,3 @@ def show(transaction_id):
         }
 
     return render_template('donations/show.html', transaction=transaction, result=result)
-
-
-@donations_blueprint.route('/', methods=['POST'])
-def create():
-    result = transact({
-        'amount': request.form['amount'],
-        'payment_method_nonce': request.form['payment_method_nonce'],
-        'options': {
-            "submit_for_settlement": True
-        }
-    })
-
-    # If transaction is successful
-    if result.is_success or result.transaction:
-        return redirect(url_for('donations.show', transaction_id=result.transaction.id))
-
-    else:
-        for x in result.errors.deep_errors:
-            flash('Error: %s: %s' % (x.code, x.message))
-        return redirect(url_for('donations.new'))
