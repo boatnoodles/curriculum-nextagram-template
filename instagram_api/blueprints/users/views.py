974a90dc2922093c3339b351a10d274441493e1e
mei <yimei.wym@gmail.com>
2019-06-02 09:37:04 +0800

    remove templates folder

--- instagram_api/blueprints/users/views.py
+++ instagram_api/blueprints/users/views.py
@@ -2,9 +2,7 @@ from flask import Blueprint, jsonify
 from models.user import User
 
 users_api_blueprint = Blueprint('users_api',
-                                __name__,
-                                template_folder='templates')
-
+                                __name__)
 
 @users_api_blueprint.route('/', methods=['GET'])
 def index():
